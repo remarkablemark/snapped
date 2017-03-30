a7c00252f75e183a44ec0843de93370db20673cd
Mark <mark@remarkablemark.org>
2017-03-30 00:42:12 -0400

    Single out a test when `only` is truthy
    This makes it easier to isolate a lib test when watching or
    debugging.

--- test/lib/tree-to-snapshot.test.js
+++ test/lib/tree-to-snapshot.test.js
@@ -11,8 +11,9 @@ import jestSnapshots from '../setup/__snapshots__/index.test.js.snap';
  * Compare Jest snapshots with `treeToSnapshot()`.
  */
 describe(treeToSnapshotTests.title, () => {
-    treeToSnapshotTests.tests.forEach(({ title, tree }) => {
-        test(title, () => {
+    treeToSnapshotTests.tests.forEach(({ title, tree, only }) => {
+        const _test = only ? test.only : test;
+        _test(title, () => {
             expect(treeToSnapshot(tree)).toEqual(jestSnapshots[`${title} 1`]);
         });
     });
